ef8f5d0321d096a93d5e27a8fd0e8cf1ad91b198
Roshan Joyce <roshan.joyce@gmail.com>
2014-09-06 19:56:20 +0530

    Fixed issue #26 Get on domain_management.domains does not return anything

---
+++ test/test_devices_in_domain.py
@@ -0,0 +1,27 @@
+import ConfigParser
+
+from jnpr.space import rest
+
+class TestLogin:
+
+    def setup_class(self):
+        # Extract Space URL, userid, password from config file
+        config = ConfigParser.RawConfigParser()
+        config.read("./test.conf")
+        url = config.get('space', 'url')
+        user = config.get('space', 'user')
+        passwd = config.get('space', 'passwd')
+
+        # Create a Space REST end point
+        self.space = rest.Space(url, user, passwd)
+
+    def test_get_devices_in_domains(self):
+        self.space.domain_management.domains.get()
+
+        ds = self.space.domain_management.domains.get()
+
+        for d in ds[0].children.domain:
+            assert d.name
+            devices_list = self.space.device_management.devices.get(filter_={'domainId': d.id})
+            for dev in devices_list:
+                print dev.name
