3d6d89dec004c534dfa79d00ac901d0cac93e589
Roshan Joyce <roshan.joyce@gmail.com>
2015-05-11 14:07:42 +0530

    Added test_filtering

---
+++ test/test_filtering.py
@@ -0,0 +1,46 @@
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+#
+# Copyright (c) 2015 Juniper Networks, Inc.
+# All rights reserved.
+#
+# Use is subject to license terms.
+#
+# Licensed under the Apache License, Version 2.0 (the ?License?); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#from __future__ import unicode_literals
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+import configparser
+
+from jnpr.space import rest
+
+class TestFiltering(object):
+
+    def setup_class(self):
+        # Extract Space URL, userid, password from config file
+        config = configparser.RawConfigParser()
+        config.read("./test.conf")
+        url = config.get('space', 'url')
+        user = config.get('space', 'user')
+        passwd = config.get('space', 'passwd')
+
+        # Create a Space REST end point
+        self.space = rest.Space(url, user, passwd)
+
+    def test_filter(self):
+        list_1 = self.space.device_management.devices.get(
+                            filter_={'managedStatus': 'In Sync'})
+        list_2 = self.space.device_management.devices.get(
+                            filter_="managedStatus eq 'In Sync'")
+        assert len(list_1) == len(list_2)
